4bb2c5ecbe85cdc8e2e6cd861990ab1d3e06c6bc
3omar63 <om728526@gmail.com>
2025-02-04 03:57:38 +0200

    Finished first two questions

---
+++ Session02Collections/Helper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session02Collections
+{
+    internal class Helper
+    {
+        #region Question1
+        public static ArrayList ReverseArrayList(ArrayList list)
+        {
+            ArrayList ReversedArray = new ArrayList(list.Capacity);
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                ReversedArray.Add(list[i]);
+            }
+            return ReversedArray;
+        }
+        #endregion
+
+        #region Question 2
+
+        public static List<int> GetEvenNumbers(List<int> Numbers)
+        {
+            List<int> EvenNumbers=new List<int>();
+
+            foreach (var item in Numbers)
+            {
+                if(item%2==0)
+                    EvenNumbers.Add(item);
+            }
+            return EvenNumbers;
+        }
+        #endregion
+
+    }
+}
---
+++ Session02Collections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Session02Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            ///1.	You are given an ArrayList containing a sequence of elements.
+            ///try to reverse the order of elements in the ArrayList in-place(in the same arrayList)
+            ///    without using the built-in Reverse.
+            ///    Implement a function that takes the ArrayList as input and modifies it to have the reversed order of elements.
+
+
+            //ArrayList arrayList = new ArrayList() { 1, 2, 3, 4, 5 };
+            //ArrayList reversedList = Helper.ReverseArrayList(arrayList);
+
+            //foreach (var item in reversedList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            #endregion
+
+            #region 2.	You are given a list of integers. Your task is to find and return a new list containing only the even numbers from the given list.
+            
+            //List<int> Numbers = new List<int> {1,2,3,4,5,6,7,8,9,10 };
+            //List<int>EvenNumbers= Helper.GetEvenNumbers(Numbers);
+            //foreach (var item in EvenNumbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+
+
+
+        }
+    }
+}
