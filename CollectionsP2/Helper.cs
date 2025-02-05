11fee2d8165f500c3b129021e4e1d4164d32c102
3omar63 <om728526@gmail.com>
2025-02-06 01:01:46 +0200

    Solved q1

---
+++ CollectionsP2/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session03CollectionP2
+{
+    public class Helper
+    {
+        #region Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+        public static void ReverseQueueElementsByStack<T>(Queue<T> q)
+        {
+            if (q is not null)
+            {
+                Stack<T> stack = new Stack<T>();
+                while (q.TryDequeue(out T element))
+                {
+                    stack.Push(element);
+                }
+
+                foreach (var item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+                Console.WriteLine("Queue Is Empty");
+        }
+        #endregion
+
+    }
+}
---
+++ CollectionsP2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Session03CollectionP2;
+using System.ComponentModel.Design;
+
+namespace CollectionsP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>(new int[]{1,2,3,4 });
+            Helper.ReverseQueueElementsByStack(q);
+        }
+    }
+}
