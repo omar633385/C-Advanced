74b9839344073094cf9827bfb6592f0eb6fe454c
3omar63 <om728526@gmail.com>
2025-02-10 23:24:22 +0200

    impelmented Book Functions

---
+++ session 4 Delegates/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session_4_Delegates
+{
+    internal class Book
+    {
+
+
+        #region Properties
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        #endregion
+        #region Constructor
+        public Book(string iSBN, string title, string[] authors, DateTime publicationDate, decimal price)
+        {
+            ISBN = iSBN;
+            Title = title;
+            Authors = authors;
+            PublicationDate = publicationDate;
+            Price = price;
+        }
+        #endregion
+        public override string ToString()
+        {
+            return $"ISBN:{ISBN} :: Title:{Title}:: Price:{Price}:: PublicationDate:{PublicationDate}";
+        }
+    }
+}
